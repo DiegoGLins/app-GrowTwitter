d79bd7f29588fcd6a15727045c456f9935392c51
Diego <dl7designer@gmail.com>
2023-10-26 20:13:53 -0300

    atualizacao do home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,29 @@
+// import { useState } from "react"
 import CardExplorer from "../components/CardExplorer"
 import CardTweet from "../components/CardTweet"
 import FeedBox from "../components/FeedBox"
 import HeaderPage from "../components/HeaderPage"
 import LayoutDefault from "../components/LayoutDefault"
 import SideExplorer from "../components/SideExplorer"
+// import { useNavigate } from "react-router-dom"
 
 
 const Home: React.FC = () => {
+  // const navigate = useNavigate()
+  // const [allTweets, setAllTweets] = useState([])
+
+  // const token = localStorage.getItem("token")
+
+
+  // if (!token) {
+  //   navigate("/")
+  //   return
+  // }
+
+  // async function getData() {
+  //   const response = await listData(token as string);
+  // }
+
   return (
 
     <div style={{ display: "flex" }}>

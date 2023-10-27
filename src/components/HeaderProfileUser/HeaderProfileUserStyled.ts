8a1f09bf041e0a00973dc86aedd25c6155df2207
Diego <dl7designer@gmail.com>
2023-10-27 18:21:59 -0300

    alteracao no HeaderProfileUserStyled

--- src/components/HeaderProfileUser/HeaderProfileUserStyled.ts
+++ src/components/HeaderProfileUser/HeaderProfileUserStyled.ts
@@ -11,13 +11,14 @@ border-radius: 100%;
 
 
 export const HeaderProfileUserStyled = styled.div`
-margin: 15px 0px 0px 15px;
+margin: 15px 0px 10px 15px;
 display: flex;
 flex-direction: column;
 background-color: #fff;
 color: #000;
 max-width: 100vw;
 height: 160px;
+position: relative;
 font-family: Arial bold, Helvetica, sans-serif;
 padding: 6px 10px 6px 10px;
 align-items: start;

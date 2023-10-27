1ad6408886e9c4538778d929654f11caa8a076d8
Diego <dl7designer@gmail.com>
2023-10-27 18:21:18 -0300

    altercao nas propriedade do FeedBoxStyled

--- src/components/FeedBox/FeedBoxStyled.ts
+++ src/components/FeedBox/FeedBoxStyled.ts
@@ -4,8 +4,8 @@ export const FeedBoxStyled = styled.div`
 background-color: #fff;
 min-height: 100vh;
 width: 65%;
-margin: 0;
-padding: 0;
+margin: 0px;
+padding: 20px 0px 0px 0px;
 border-right: 2px solid #e9e9e9;
 border-left: 2px solid #e9e9e9;
 display: flex;

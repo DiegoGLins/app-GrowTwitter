24d4554eb511d92c3f536840d021e129366050d5
Diego <dl7designer@gmail.com>
2023-10-27 18:20:21 -0300

    alteracao no IconsTweet

--- src/components/CardTweet/IconsTweet.tsx
+++ src/components/CardTweet/IconsTweet.tsx
@@ -1,25 +1,18 @@
-import { IconsTweetStyled } from "./IconsTweetStyled";
+
 import '../../App.css'
 
 interface IconTweetProps {
-    iconComent: string;
-    iconLike: string
-    countComent: number;
-    countLike: number
+    icon: string;
+    count: number;
     children?: React.ReactNode;
 }
 
-const IconsTweet: React.FC<IconTweetProps> = ({ iconComent, iconLike, countComent, countLike }) => {
+const IconsTweet: React.FC<IconTweetProps> = ({ icon, count, }) => {
+
     return (
         <>
-            <IconsTweetStyled>
-                <img className='iconComent' style={{ height: '18px', width: '18x' }} src={iconComent} alt="iconTweetComent"></img>
-                <div style={{ margin: '0px 0px 0px 8px', fontSize: '16px' }}>{countComent}</div>
-            </IconsTweetStyled>
-            <IconsTweetStyled>
-                <img className='iconLike' style={{ height: '17px', width: '17px' }} src={iconLike} alt="iconTweetLike"></img>
-                <div style={{ margin: '0px 0px 0px 8px', fontSize: '16px' }}>{countLike}</div>
-            </IconsTweetStyled>
+            <img className='iconResponse' style={{ height: '18px', width: '18x' }} src={icon} alt="iconTweetComent"></img>
+            <div style={{ margin: '0px 0px 0px 18px', fontSize: '16px' }}>{count}</div>
         </>
     )
 }

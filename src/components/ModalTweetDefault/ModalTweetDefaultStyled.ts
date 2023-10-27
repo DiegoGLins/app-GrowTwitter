32eb295c618493ac5f4b14cde656c45543d66e2c
Diego <dl7designer@gmail.com>
2023-10-27 18:23:49 -0300

    cricacao do ModalTweetDefaultStyled

---
+++ src/components/ModalTweetDefault/ModalTweetDefaultStyled.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const ModalTweetDefaultStyled = styled.div`
+background-color: #fff;
+max-width: 450px;
+max-height: 300px;
+overflow: hidden;
+border-radius: 16px;
+border: 1px solid inset;
+`

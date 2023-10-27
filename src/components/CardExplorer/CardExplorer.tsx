2e9e440df4411942f0e5890af994a0503491c800
Diego <dl7designer@gmail.com>
2023-10-27 18:17:16 -0300

    card explorer atualizado

--- src/components/CardExplorer/CardExplorer.tsx
+++ src/components/CardExplorer/CardExplorer.tsx
@@ -8,6 +8,17 @@ const CardExplorer: React.FC<CardExplorerProps> = ({ children }) => {
     return (
         <>
             <CardExplorerStyled>
+                <h4><strong>O que está acontecendo ?</strong></h4>
+                <h5 style={{ color: 'gray', paddingTop: '15px' }}>Esportes - Há 45 minutos</h5>
+                <p><strong>Assunto sobre esportes</strong></p>
+                <h5 style={{ color: 'gray', paddingTop: '15px' }}>Assunto do Momento em Brasil</h5>
+                <p><strong>Assunto do Momento</strong></p>
+                <h5 style={{ color: 'gray', paddingTop: '15px' }}>Música - Há 25 minutos</h5>
+                <p><strong>Assunto sobre Música</strong></p>
+                <h5 style={{ color: 'gray', paddingTop: '15px' }}>Filmes - Há 15 minutos</h5>
+                <p><strong>Assunto sobre Filmes</strong></p>
+                <h5 style={{ color: 'gray', paddingTop: '15px' }}>Notícias - Agora pouco</h5>
+                <p><strong>Notícias de última hora</strong></p>
                 {children}
             </CardExplorerStyled>
         </>

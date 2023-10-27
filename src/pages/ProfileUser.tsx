5eca5cca0b4a30cb487cd943082f5871d7e6e4e1
Diego <dl7designer@gmail.com>
2023-10-27 18:25:49 -0300

    alteracao no profileUser ainda em revisao

--- src/pages/ProfileUser.tsx
+++ src/pages/ProfileUser.tsx
@@ -6,9 +6,13 @@ import LayoutDefault from "../components/LayoutDefault"
 import SideExplorer from "../components/SideExplorer"
 
 const ProfilelUser: React.FC = () => {
+
+  //logica para receber informações dos tweets enviados pelo usuario logado passando as informações
+  //para atualizar o cardTweet e o Header do usuario
+
   return (
 
-    <div style={{ display: "flex" }}>
+    <div style={{ display: "flex", maxHeight: '100%', overflow: 'auto' }}>
       <LayoutDefault />
       <FeedBox>
         <HeaderProfileUser avatar={""} nameUser={""} usernameUser={""} />

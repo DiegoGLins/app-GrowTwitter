84fca26ef1fd65cd50223ebb3953cf027476d928
Diego <dl7designer@gmail.com>
2023-10-27 18:24:11 -0300

    alteracao do TogleMenu

--- src/components/TogleMenu/TogleMenu.tsx
+++ src/components/TogleMenu/TogleMenu.tsx
@@ -1,12 +1,41 @@
 import { TogleMenuStyled } from "./TogleMenuStyled";
-
+import iconePaginaInicial from '/icone_pagina_Inicial.svg'
+import iconeExplorar from '/icone_explorar.svg'
+import iconePerfilSelecionado from '/icone_perfil_selecionado.svg'
+import IconTogleMenu from '../IconTogleMenu'
+import { IconTogleMenuStyled } from "../IconTogleMenu/IconTogleMenuStyled";
+import { useNavigate } from "react-router-dom";
 interface TogleMenuProps {
     children?: React.ReactNode;
 }
 
 const TogleMenu: React.FC<TogleMenuProps> = ({ children }) => {
+    const navigate = useNavigate()
+
+    const navigateProfileUser = () => {
+        navigate('/profile-user')
+    }
+
+    const navigateHome = () => {
+        navigate('/home')
+    }
+
+    const navigateExplorer = () => {
+        navigate('/explorer')
+    }
+
+
     return (
         <TogleMenuStyled>
+            <IconTogleMenuStyled onClick={navigateHome} className="iconTogleButton">
+                <IconTogleMenu icon={iconePaginaInicial} title={"Pagina Inicial"} />
+            </IconTogleMenuStyled>
+            <IconTogleMenuStyled onClick={navigateExplorer} className="iconTogleButton">
+                <IconTogleMenu icon={iconeExplorar} title={"Explorar"} />
+            </IconTogleMenuStyled>
+            <IconTogleMenuStyled onClick={navigateProfileUser} className="iconTogleButton">
+                <IconTogleMenu icon={iconePerfilSelecionado} title={"Perfil"} />
+            </IconTogleMenuStyled>
             {children}
         </TogleMenuStyled>
     )

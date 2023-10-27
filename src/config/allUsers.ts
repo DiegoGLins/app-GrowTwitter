e42f2ede1ea7c4f70199f5e6d098af839a0d3517
Diego <dl7designer@gmail.com>
2023-10-27 18:24:28 -0300

    allUsers em revisao

---
+++ src/config/allUsers.ts
@@ -0,0 +1,8 @@
+import { UserDto } from "./services/user.service";
+
+
+const allUsers: UserDto[] = [
+    { id: '', name: '', email: '', token: '', username: '', password: '', tweets: [], reTweet: [], followers: [], following: [] },
+]
+
+export default allUsers

b5e77c24f9ee29a4037aff6c7252a26071f01dc2
Diego <dl7designer@gmail.com>
2023-10-27 21:48:25 -0300

    alteracao da api service

--- src/config/services/api.service.ts
+++ src/config/services/api.service.ts
@@ -2,7 +2,10 @@
 import axios from 'axios';
 
 const apiService = axios.create({
-    baseURL: 'https://api-growtwitter-6edn.onrender.com'
+    baseURL: 'https://api-growtwitter-6edn.onrender.com',
+    headers: {
+        Authorization: localStorage.getItem('token')
+    }
 })
 
 export default apiService

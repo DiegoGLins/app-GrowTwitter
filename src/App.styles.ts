417f661125d0a2e4e23fac90825f403653acd9d5
Diego <dl7designer@gmail.com>
2023-10-27 21:45:38 -0300

    alteracao no css dos containers

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -2,14 +2,14 @@ import styled from "styled-components";
 
 export const ContatinerLayoutDefault = styled.div`
 background-color: #fff;
-color: '#0000';
-min-height: 100%;
+min-height: 100vh;
 min-width: 100%;
 margin: 0;
 padding: 0;
 display: flex;
 flex-direction: column;
 position: relative;
+justify-content: stretch;
 `
 export const ContainerSideBarDefault = styled.div`
 border-right: 2px solid #e9e9e9;
@@ -18,4 +18,18 @@ min-width: 100%;
 display: flex;
 flex-direction: column;
 position: relative;
+justify-content: flex-start;
+`
+export const ContainerFooter = styled.div`
+background-color: #fff;
+min-height: 100%;
+max-width: 100%;
+margin: 0;
+padding: 0;
+display: flex;
+flex-direction: column;
+position: relative;
+padding-bottom: 15px;
+justify-content: flex-end
+
 `

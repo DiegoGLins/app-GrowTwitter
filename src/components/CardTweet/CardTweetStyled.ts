55f6e15d9c6536bbc66d7d3228b4dad48fc870b3
Diego <dl7designer@gmail.com>
2023-10-28 17:36:16 -0300

    alteracao no CardTweetStyled

--- src/components/CardTweet/CardTweetStyled.ts
+++ src/components/CardTweet/CardTweetStyled.ts
@@ -1,27 +1,33 @@
 import styled from "styled-components";
 
-export const AvatarTweet = styled.img`
-background-image: url();
-margin-bottom: 20px;
-height: 45px;
-width: 45px;
+export const AvatarTweet = styled.div`
+margin: 10px 0px 0px 5px;
+min-height: 35px;
+min-width: 35px;
 border: 1px gold solid;
 border-radius: 100%;
+overflow: visible;
+
+>img {
+    border-radius: 100%;
+    min-height: 35px;
+    min-width: 35px;
+}
 `
 
 export const CardTweetStyled = styled.div`
-margin: 2px;
-padding-left: 20px;
-max-width: 100vw;
+max-width: 100%;
+max-height: 100%;
 display: flex;
 justify-content: start;
-align-items: center;
+align-items: flex-start;
 background-color: #fff;
-border: 2px solid #e9e9e9;
+border-top: 1px solid #e9e9e9;
+border-bottom: 1px solid #e9e9e9;
 border-bottom-style: inset;
 color: #000;
-height: 110px;
-overflow-y: hidden;
-border-radius: 16px;
+padding: 0px 0px 15px 5px;
+overflow: visible;
 position: relative;
+
 `

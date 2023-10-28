5639f4b71080b8e094ca3879f4305d1ad3ca85aa
Diego <dl7designer@gmail.com>
2023-10-27 21:47:18 -0300

    alteracao no SideBarStyled

--- src/components/SideBar/SideBarStyled.ts
+++ src/components/SideBar/SideBarStyled.ts
@@ -2,7 +2,6 @@ import styled from "styled-components";
 
 export const SideBarStyled = styled.div`
 background-color: #fff;
-color: '#0000';
 min-height: 100vh;
 max-width: 240px;
 margin: 0;
@@ -11,5 +10,6 @@ display: flex;
 flex-direction: column;
 position: relative;
 padding-bottom: 15px;
+justify-content: flex-start
 `
 

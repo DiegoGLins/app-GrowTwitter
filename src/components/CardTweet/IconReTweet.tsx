c1d39aaf613191f7305f91c0042617dd71396fae
Diego <dl7designer@gmail.com>
2023-10-28 19:03:34 -0300

    alteracao no iconReTweet

--- src/components/CardTweet/IconReTweet.tsx
+++ src/components/CardTweet/IconReTweet.tsx
@@ -10,7 +10,7 @@ const IconReTweet: React.FC<IconReTweetProps> = ({ iconReTweet, count, }) => {
     return (
         <>
             <img className='iconsTweet' style={{ height: '18px', width: '18x' }} src={iconReTweet} alt="ReTweet"></img>
-            <div style={{ margin: '0px 0px 0px 18px', fontSize: '16px' }}>{count}</div>
+            <div style={{ margin: '0px 0px 0px 8px', fontSize: '16px' }}>{count}</div>
         </>
     )
 }

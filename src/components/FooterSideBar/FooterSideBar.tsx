0ca91f9914ea3f3023ee9084e2a0aa513c97c31e
Diego <dl7designer@gmail.com>
2023-10-27 21:45:58 -0300

    funcao de logout criada

--- src/components/FooterSideBar/FooterSideBar.tsx
+++ src/components/FooterSideBar/FooterSideBar.tsx
@@ -1,17 +1,39 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { useNavigate } from "react-router";
 import { FooterButton, FooterSideBarStyled } from "./FooterSidebarStyled";
+import { logout } from "../../config/services/auth.service";
+import { useState } from "react";
 
 interface FooterSideBarProps {
     avatar: string;
     nameUser: string;
     usernameUser: string;
+    children?: React.ReactNode
 }
 
 const FooterSideBar: React.FC<FooterSideBarProps> = ({ avatar, nameUser, usernameUser }) => {
+    const [alert, setAlert] = useState('')
+
+    const navigate = useNavigate()
+
+    const logged = localStorage.getItem('token')
+
+    async function logoutUser() {
+
+        if (logged) {
+            const response = await logout(logged)
+            setAlert(response.message!)
+            localStorage.setItem('token', '')
+            navigate('/')
+        }
+    }
+
+
     return (
         <FooterSideBarStyled>
             <div style={{ margin: '5px 0px 15px 5px', display: 'flex', flexDirection: 'column' }}>
                 <img className="avatarFooter" src={avatar} alt='avatar-footer'></img>
-                <FooterButton className="logout">Sair</FooterButton>
+                <FooterButton onClick={logoutUser} className="logout">Sair</FooterButton>
             </div>
             <div style={{ margin: '10px 0px 5px 5px' }}>
                 <p style={{ border: '1px solid #f015', width: '95px', height: '20px' }}>{nameUser}</p>

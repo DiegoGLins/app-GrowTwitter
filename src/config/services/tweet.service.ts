ce50e4770117fb9ce3bef5a70c1cda39c725ccd1
Diego <dl7designer@gmail.com>
2023-10-27 21:48:59 -0300

    alteracao do tweet service

--- src/config/services/tweet.service.ts
+++ src/config/services/tweet.service.ts
@@ -2,33 +2,23 @@
 import apiService, { ResponseApi } from "./api.service";
 
 export interface TweetDto {
+    idTweet: string;
     idUser?: string
     nameUser?: string
     usernameAuthorTweet: string
     content: string;
 }
 
-// export interface UpdateTweetDto {
-//     idUser: string;
-//     idTweet: string;
-//     content?: string
-// }
-
 export interface CreateTweetRequest {
+    idUser?: string;
+    nameUser?: string;
+    usernameAuthorTweet?: string
     content: string;
-    idUser: string;
-    nameUser: string;
-    usernameAuthorTweet: string
     token: string
 }
 
-export interface UpdateTweetRequest {
-    id: string;
-    content?: string;
-    idUser?: string
-    authorTweet?: string
-    token: string
-}
+
+type UpdateTweetRequest = Partial<CreateTweetRequest> & { idTweet: string }
 
 export interface DeleteTweetRequest {
     id: string;
@@ -96,11 +86,32 @@ export async function listAll(token: string) {
     }
 }
 
+// export async function listById(idTweet: string) {
+//     try {
+//         const resposta = await apiService.get(`/tweets/${idTweet} `, {
+//         });
+
+//         return {
+//             ok: resposta.data?.ok,
+//             message: resposta.data.message,
+//             code: resposta.data.code,
+//             data: resposta.data.data,
+//         };
+//     } catch (error: any) {
+//         return {
+//             ok: error.response.data?.ok,
+//             code: error.response.data?.code,
+//             message: error.response.data?.message,
+//             data: error.response.data?.data,
+//         };
+//     }
+// }
+
 export async function update(data: UpdateTweetRequest) {
     try {
         const body = { tweet: data.content };
 
-        const resposta = await apiService.put(`/tweets/${data.id}`, body, {
+        const resposta = await apiService.put(`/tweets/${data.idTweet}`, body, {
             headers: {
                 Authorization: data.token,
             },
@@ -111,6 +122,27 @@ export async function update(data: UpdateTweetRequest) {
             code: resposta.data?.code,
             message: resposta.data?.message,
             data: resposta.data?.data,
+        };
+    } catch (error: any) {
+        return {
+            ok: error.response.data?.ok,
+            code: error.response.data?.code,
+            message: error.response.data?.message,
+            data: error.response.data?.data,
+        };
+    }
+}
+
+export async function deleteTweet(idTweet: string) {
+    try {
+        const resposta = await apiService.delete(`/tweets/${idTweet} `, {
+        });
+
+        return {
+            ok: resposta.data?.ok,
+            message: resposta.data.message,
+            code: resposta.data.code,
+            data: resposta.data.data,
         };
     } catch (error: any) {
         return {

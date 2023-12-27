64b44831961469c0971e23b79b9dee171f1120e1
Diego <dl7designer@gmail.com>
2023-12-27 00:59:11 -0300

    alteracao no apptsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
+import { Provider } from 'react-redux'
 import './App.css'
 import DefaultTheme from './config/theme/DefaultTheme'
 import RoutesApp from './routes/RoutesApp'
+import store from './store'
 
 const App: React.FC = () => {
 
   return (
     <DefaultTheme>
-      <RoutesApp />
+      <Provider store={store}>
+        <RoutesApp />
+      </Provider>
     </DefaultTheme>
   )
 }
